ed4b23e353d121e4db7673c7251c0b24558ead6d
Andreja Jevtic <andrajevtic@gmail.com>
2015-12-28 14:50:55 -0800

    Added Palette library, and enabled extracting colors from disc images

--- app/src/main/java/com/example/futin/importimages/UserInterface/adapters/SingleImageAdapter.java
+++ app/src/main/java/com/example/futin/importimages/UserInterface/adapters/SingleImageAdapter.java
@@ -3,7 +3,9 @@ package com.example.futin.importimages.UserInterface.adapters;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v4.view.PagerAdapter;
+import android.support.v7.graphics.Palette;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,8 @@ import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareButton;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by Futin on 12/25/2015.
@@ -29,7 +33,8 @@ public class SingleImageAdapter extends PagerAdapter {
     LayoutInflater inflater;
     ListChangeListener listener;
     int listSize=0;
-
+    Button btn_00, btn_01,btn_02, btn_10, btn_11, btn_12;
+    Map<String, ArrayList<Palette.Swatch>>colorsMap;
     public SingleImageAdapter(Context context) {
         this.context=context;
         listener= (ListChangeListener) context;
@@ -60,6 +65,8 @@ public class SingleImageAdapter extends PagerAdapter {
         ShareButton shareButton= (ShareButton) viewLayout.findViewById(R.id.shareButton);
         btnDelete= (Button) viewLayout.findViewById(R.id.btnDelete);
         String fileName=ListHolder.getInstance().returnFileName(position);
+        initButtons(viewLayout, getColorRGB(fileName), fileName);
+
         String AbsoluteFilePath=ListHolder.getInstance().getFilePath()+'/';
 
         final String filePath=AbsoluteFilePath+fileName;
@@ -96,6 +103,77 @@ public class SingleImageAdapter extends PagerAdapter {
         container.removeView((View) object);
     }
 
+
+
+    void makeToast(String text){
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public int getItemPosition(Object object){
+        return PagerAdapter.POSITION_NONE;
+    }
+
+    void initButtons(View view, ArrayList<Integer> colors, String url){
+        btn_00= (Button) view.findViewById(R.id.btn_00);
+        btn_01= (Button) view.findViewById(R.id.btn_01);
+        btn_02= (Button) view.findViewById(R.id.btn_02);
+        btn_10= (Button) view.findViewById(R.id.btn_10);
+        btn_11= (Button) view.findViewById(R.id.btn_11);
+        btn_12= (Button) view.findViewById(R.id.btn_12);
+
+        if(colors.size() > 0 && colorsMap.containsKey(url)) {
+            setButtonColors(colors);
+        }
+    }
+    void setButtonColors(ArrayList<Integer> colors){
+        for(int i=0;i<colors.size();i++) {
+            switch (i) {
+                case 0:
+                    setDrawableBackground(btn_00,i,colors);
+                    break;
+                case 1:
+                    setDrawableBackground(btn_01,i,colors);
+
+                    break;
+                case 2:
+                    setDrawableBackground(btn_02,i,colors);
+
+                    break;
+                case 3:
+                    setDrawableBackground(btn_10,i,colors);
+
+                    break;
+                case 4:
+                    setDrawableBackground(btn_11,i,colors);
+                    break;
+                case 5:
+                    setDrawableBackground(btn_12,i,colors);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void setDrawableBackground(Button btn,int i, ArrayList<Integer> colors){
+        GradientDrawable gd= (GradientDrawable) btn.getBackground();
+        gd.setColor(colors.get(i));
+        btn.setVisibility(View.VISIBLE);
+
+    }
+
+
+    ArrayList<Integer> getColorRGB (String fileName){
+        ArrayList<Integer> listOfColors=new ArrayList<>();
+        colorsMap=ListHolder.getInstance().getColorsMap();
+
+        if(colorsMap != null && colorsMap.containsKey(fileName)){
+            for(Palette.Swatch swatch : colorsMap.get(fileName)){
+                listOfColors.add(swatch.getRgb());
+            }
+        }
+        return listOfColors;
+    }
     /*
         Method used for sharing single photo on Facebook
     */
@@ -107,14 +185,6 @@ public class SingleImageAdapter extends PagerAdapter {
                 .addPhoto(photo)
                 .build();
         return content;
-    }
-
-    void makeToast(String text){
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    public int getItemPosition(Object object){
-        return PagerAdapter.POSITION_NONE;
     }
 
 }
--- app/src/main/java/com/example/futin/importimages/UserInterface/controller/ListHolder.java
+++ app/src/main/java/com/example/futin/importimages/UserInterface/controller/ListHolder.java
@@ -1,11 +1,14 @@
 package com.example.futin.importimages.UserInterface.controller;
 
+import android.support.v7.graphics.Palette;
+
 import com.example.futin.importimages.RestService.cache.FileCache;
 import com.example.futin.importimages.RestService.models.Image;
 import com.example.futin.importimages.UserInterface.adapters.GridViewAdapter;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by Futin on 12/25/2015.
@@ -19,6 +22,8 @@ public class ListHolder {
     ArrayList<String>listOfFiles;
     FileCache fileDir;
     GridViewAdapter grid;
+
+    Map<String, ArrayList<Palette.Swatch>> colorsMap;
     /*
         Singleton class, designed for list manipulation, end sending data between activities
     */
@@ -38,6 +43,22 @@ public class ListHolder {
         this.grid=grid;
     }
 
+    public void setFileDir(FileCache file){
+        fileDir=file;
+        convertFilesToList();
+    }
+    public void setColorsMap(Map<String, ArrayList<Palette.Swatch>> colorsMap){
+        this.colorsMap=colorsMap;
+    }
+    public Map<String, ArrayList<Palette.Swatch>> getColorsMap(){
+        return colorsMap;
+    }
+
+    public void addToFiles(String url){
+        if(!listOfFiles.contains(url))
+            listOfFiles.add(url);
+    }
+
     public void notifyGrid(){
         grid.notifyGrid(fileDir, true);
         resetFiles();
@@ -50,18 +71,10 @@ public class ListHolder {
         }
     }
 
-
-
     public void removeFileFromList(int position){
         if(position<listOfFiles.size()){
             listOfFiles.remove(position);
         }
-    }
-
-
-    public void setFileDir(FileCache file){
-        fileDir=file;
-        convertFilesToList();
     }
 
     void convertFilesToList(){
@@ -72,24 +85,18 @@ public class ListHolder {
         }
     }
 
-    public void addToFiles(String url){
-        if(!listOfFiles.contains(url))
-            listOfFiles.add(url);
-    }
-    public ArrayList<String> getListOfFiles(){
-        return listOfFiles;
-    }
-
     public String returnFileName(int position){
 
         return listOfFiles.get(position);
     }
+
     /*
         Method for getting absolute path to our humanityFiles directory
     */
     public String getFilePath(){
         return fileDir.getDirectory().getAbsolutePath();
     }
+
     /*
         Calculation for getCount method
     */
